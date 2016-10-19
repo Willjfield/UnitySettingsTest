a1b0e5a54ca8c75920fbe3d9fb5cf5da45a7c400
Willjfield <willjfield@gmail.com>
2016-10-19 15:08:23 -0400

    settings test inital

---
+++ Assets/RandomRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (.1f,.2f,.1f);
+	}
+}
